2eb0ff014eb802e8c1ac1b64a5445e3fe850e307
644731871zzz <644731871zzz@gmail.com>
2025-11-28 22:00:17 +0800

    20251128

--- book1/ch07/test.py
+++ book1/ch07/test.py
@@ -8,7 +8,8 @@ current_dir = os.path.dirname(current_script_path)
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from common.util import im2col
+from common.functions import *
+from common.util import im2col, col2im
 
 x1 = np.random.rand(1,3,7,7)
 col1 = im2col(x1,5,5,stride = 1,pad = 0)
@@ -23,11 +24,42 @@ class Convolution:
         self.stride = stride
         self.pad = pad
 
+        #中间数据(反向传播用)
+        self.x = None
+        self.col = None
+        self.col_W = None
+        
+        #权重和偏置参数的梯度
+        self.dW = None
+        self.db = None
+
     def forward(self,x):
         FN,C,FH,FW = self.W.shape
         N,C,H,W = x.shape
         out_h = int(1 + (H + 2*self.pad - FH)/self.stride)
         out_w = int(1 + (W + 2*self.pad - FW)/self.stride)
 
-        
-        
+        col = im2col(x,FH,FW,self.strid,self.pad)
+        col_W = self.W.reshape(FN,-1).T#滤波器展开,转置是为了滤波器左乘数据
+        out = np.dot(col,col_W) + self.b
+
+        out = out.reshape(N,out_h,out_w,-1).transpose(0,3,1,2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+    
+    def backward(self,dout):
+        FN,C,FH,FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1,FN)
+
+        self.db = np.sum(dout,axis = 0)
+        self.dW = np.dot(self.col.T,dout)
+        self.dW = self.dW.transpose(1,0).reshape(FN,C,FH,FW)
+
+        dcol = np.dot(dout,self.col_W.T)
+        dx = col2im(dcol,self.x.shape,FH,FW,self.stride,self.pad)
+
+        return dx
