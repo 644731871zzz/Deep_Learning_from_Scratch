2d760191659299fce3c6dc2ca775a3993140043e
644731871zzz <644731871zzz@gmail.com>
2026-01-24 22:06:18 +0800

    20260124

---
+++ book2/ch02/util_exersice.py
@@ -0,0 +1,25 @@
+import os,sys
+current_script_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_script_path)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+import os
+from common.np import *
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.',' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus,word_to_id,id_to_word
