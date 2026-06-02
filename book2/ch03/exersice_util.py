670099a68251cbd5eec0c88b81cdea8c6c122ebf
644731871zzz <644731871zzz@gmail.com>
2026-06-02 22:03:38 +0800

    20260602

---
+++ book2/ch03/exersice_util.py
@@ -0,0 +1,22 @@
+import os,sys
+current_script_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_script_path)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from common.np import *
+
+
+def create_contexts_target(corpus,window_size = 1):
+    target = corpus[window_size: - window_size] #切片不包含终点,到-window_size之前一个结束,想取到最后一个直接[a:]不是[a:-1]
+    contexts = []
+
+    for idx in range(window_size,len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size,window_size + 1):
+            if t == 0:
+                continue #
+            cs.append(corpus[idx + t])
+        contexts.append(cs) #
+
+    return np.array(contexts),np.array(target)
