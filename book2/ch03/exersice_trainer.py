901bbd8e89c809a500a11351a91e87825cf7d31e
644731871zzz <644731871zzz@gmail.com>
2026-06-08 22:21:48 +0800

    20260608

--- book2/ch03/exersice_trainer.py
+++ book2/ch03/exersice_trainer.py
@@ -28,15 +28,23 @@ def remove_duplicate(params,grads):
                     params.pop(j) #pop()是删除这个对应元素,并返回这个元素,这里没接受返回
                     grads.pop(j)
 
+
+                # 如果 params[i] 和 params[j] 都是二维矩阵，
+                # 并且 params[i] 转置后的形状等于 params[j]，
+                # 并且 params[i] 转置后的所有数值都等于 params[j]，
+                # 那么认为它们是同一个权重的转置重复。
+                #all判断里面是不是都是True
                 elif params[i].ndim ==2 and params[j].ndim ==2 and \
-                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]): #
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
                     grads[i] += grads[j].T
                     find_flg = True
                     #只是修改索引数值的外部维度怎么做,不是修改内部数值(内部数值修改全都变了)
                     params.pop(j) 
                     grads.pop(j)
 
-                if find_flg:break #break结束整个循环,continue是结束当前循环
+                #break结束并跳出当前循环
+                #continue是结束并继续进入下一轮的当前循环
+                if find_flg:break 
             if find_flg:break
 
         if not find_flg:break
