d73009cef26c56bf63b45b3f50ae9e570538c5cc
644731871zzz <644731871zzz@gmail.com>
2026-06-12 21:58:32 +0800

    20260612

--- book2/ch04/negative_sampling_layer.py
+++ book2/ch04/negative_sampling_layer.py
@@ -38,7 +38,7 @@ class UnigramSampler:
         power是对词做平滑的参数
         sample_size是每次采样几个样本
         """
-        self.sample_size = sample_size#
+        self.sample_size = sample_size#负例词大小
         self.vocab_size = None #词汇表大小 占位
         self.word_p = None #每个词的概率 占位
 
@@ -65,20 +65,64 @@ class UnigramSampler:
 
         if not GPU:
             negative_sample = np.zeros((batch_size,self.sample_size),
-                                       type = np.int32)
+                                       dtype = np.int32)
 
             for i in range(batch_size):
                 p = self.word_p.copy()
                 target_idx = target[i]
                 p[target_idx] = 0
-                p /= p.sum()
-                negative_sample[i,:] = np.random.choice(self.vocab_size,
-                                                        size = self.sample_size,
-                                                        replace = False,
-                                                        p = p)
+                p /= p.sum() #处理不带当前词汇的词频
+                negative_sample[i,:] = np.random.choice(self.vocab_size, #从这里抽
+                                                        size = self.sample_size,#抽多少个
+                                                        replace = False,#不放回
+                                                        p = p)#对应概率
         else:
             negative_sample = np.random.choice(self.vocab_size,
                                                size = (batch_size,self.sample_size),
                                                replace=True,
                                                p = self.word_p)
         return negative_sample
+    
+
+class NegativeSamplingLoss:
+    def __init__(self,W,corpus,power = 0.75,sample_size = 5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus,power,sample_size)
+        #+1是因为正例的层数多一个
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+
+        self.params,self.grads = [],[]
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    #假设layers的第0个用于正例
+    def forward(self,h,target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+        
+        #正例正向传播
+        score = self.embed_dot_layers[0].forward(h,target)
+        correct_label = np.ones(batch_size,dtype = np.int32)
+        loss = self.loss_layers[0].forward(score,correct_label)
+
+        #负例的正向传播
+        negative_label = np.zeros(batch_size,dtype= np.int32)
+        for i in range(self.sample_size):
+            #取出 batch 中每个样本的第 1 个负例
+            negative_target = negative_sample[:,i]
+            #这里的h和么个样本的第一个负例的大小对应
+            score = self.embed_dot_layers[1+i].forward(h,negative_target)
+            loss += self.loss_layers[1+i].forward(score,negative_label)
+
+        return loss
+    
+
+    def backward(self,dout = 1):
+        dh = 0
+        for l0,l1 in zip(self.loss_layers,self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+
+        return dh
